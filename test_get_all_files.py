adede8155c274407705ada93b9bdc1d13fda3fe7
Sébastien Chapman <sebastien.chapman@FR-LEV1-MFG0002.local>
2020-03-10 11:42:18 +0100

    Removed unuseful tests

--- test_get_all_files.py
+++ test_get_all_files.py
@@ -5,45 +5,29 @@ from get_all_films import *
 #All URLs
 imdb_url = "https://www.imdb.com/"
 test_list_url = 'https://www.imdb.com/list/ls092117658/'
-big_test_list_url = 'https://www.imdb.com/list/ls002448041/?sort=list'\
-					'_order,asc&st_dt=&mode=detail&page=1'
-ratings_url = ('https://www.imdb.com/user/ur51298763/ratings?sort=date_'\
-				'added%2Cdesc&mode=detail&paginationKey=mfq5ijak6z7uymj'\
-				'wuuwsomnsegl34knnqsdztp6xeepepyyfxdfiwpol52uhtjimq3iwc'\
-				'lnm7gq7uk2y4kjyc3qnmn3dio7t7rbxu5m47iyfrvleknv4axfhhxu'\
-				'djs5nyx6ixac3qfdajjg6bqac2wheaznk2ouqhjumdq6vdrv2pd4x3'\
-				'rats7f7appdrvwvo4p2yvgwhiztfo3u6u&lastPosition=0')
+ratings_url = 'https://www.imdb.com/user/ur51298763/ratings'
+rating_next_url = 'https://www.imdb.com/user/ur51298763/ratings?sort='\
+					'date_added%2Cdesc&mode=detail&paginationKey=mfq5'\
+					'ijak6z7uymjwuuwsomnsegl34knnqsdztp6xeepepyyfxdfi'\
+					'wpol52uhtjimq3iwclnm7gq7uk2y4kjyayykmnytcmxy6rdx'\
+					'y4447iyfrvleknv4axfhhxudjs5nyx7yfaczqzbr564pajlx'\
+					'uqffl4f7yiuinvqyrr6wc5vkrduszupds7ann5wibi674uhs'\
+					'm3nia3ly7bqr&lastPosition=100'
+
+
 #Tests whether imdb is up and the connection works
 def test_connection_to_imdb():
 	assert get(imdb_url).status_code == 200
 	
-def test_round_down_nearest_100_243():
-	assert round_down_nearest_100(243) == 200
-
-def test_find_upper_bound(): 
-	assert find_upper_bound(1504) == 1500
-
-def test_find_upper_bound_no_film():
-	assert find_upper_bound(0) == 0
-
-def test_find_upper_bound_exact_100():
-	assert find_upper_bound(1500) == 1400
-
 def test_get_movie_ids_from_page():
 	ids = ['tt1675434', 'tt0482571', 'tt0055928']
 	assert get_movie_ids_from_page(get_soup(test_list_url)) == ids
 
-def test_get_total_number_of_movies_in_list():
-	assert get_total_number_of_movies(get_soup(test_list_url), is_ratings(test_list_url)) == 3
+def test_get_next_page_url():
+	pass
 
-def test_get_total_number_of_movies_in_list_when_more_than_999():
-	assert get_total_number_of_movies(get_soup(big_test_list_url), is_ratings(big_test_list_url)) == 1012
+def test_is_there_next_page_yes():
+	assert is_there_next_page(get_soup(ratings_url)) == True
 
-def test_is_ratings_negative():
-	assert is_ratings(test_list_url) == False
-
-def test_is_ratings_positive():
-	assert is_ratings(ratings_url) == True
-
-def test_get_total_number_of_movies_in_ratings():
-	assert get_total_number_of_movies(get_soup(ratings_url), is_ratings(ratings_url)) == 1504
+def test_is_there_next_page_no():
+	assert is_there_next_page(get_soup(test_list_url)) == False
