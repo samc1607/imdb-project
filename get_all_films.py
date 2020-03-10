67b5055b44816aa0d5da43158bcde78fa454527a
Sébastien Chapman <sebastien.chapman@FR-LEV1-MFG0002.local>
2020-03-10 15:47:38 +0100

    Refactored code

--- get_all_films.py
+++ get_all_films.py
@@ -4,22 +4,27 @@ from time import sleep
 from random import randint
 import re
 import math
+import csv
+
 
 imdb_url = 'https://www.imdb.com'
-url = 'https://www.imdb.com/user/ur51298763/ratings'
+#url = 'https://www.imdb.com/user/ur51298763/ratings'
 
-def main():
+
+def get_movies_from_url(url):
 	movie_ids = []
 	soup = get_soup(url)
 	movie_ids.extend(get_movie_ids_from_page(soup))
 	while is_there_next_page(soup):
 		soup = get_soup(get_next_page_url(soup))
 		movie_ids.extend(get_movie_ids_from_page(soup))
-	print(movie_ids)
+	return movie_ids
+
 
 def get_soup(url):
 	response = get(url)
 	return BeautifulSoup(response.text, 'html.parser')
+
 
 #Gets all the movie ids from a specific imdb page
 def get_movie_ids_from_page(soup):
@@ -29,15 +34,14 @@ def get_movie_ids_from_page(soup):
 		movie_ids.append(movie['data-tconst'])
 	return movie_ids
 
+
 def get_next_page_url(soup):
 	return imdb_url + soup.find('a', class_ = 'flat-button lister-page-next next-page')['href']
+
 
 def is_there_next_page(soup):
 	if soup.find('a', class_ = 'flat-button lister-page-next next-page') is not None:
 		return True
 	else:
 		return False
-
-if __name__== "__main__":
-  main()
 
