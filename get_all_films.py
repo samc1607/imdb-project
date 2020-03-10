63407513b8d2fbacec2a3b55afe0c5bb6d5e6d88
Sébastien Chapman <sebastien.chapman@FR-LEV1-MFG0002.local>
2020-03-10 15:56:41 +0100

    Added comments

--- get_all_films.py
+++ get_all_films.py
@@ -2,15 +2,13 @@ from requests import get
 from bs4 import BeautifulSoup
 from time import sleep
 from random import randint
-import re
-import math
-import csv
 
 
 imdb_url = 'https://www.imdb.com'
-#url = 'https://www.imdb.com/user/ur51298763/ratings'
 
 
+#Gets all the movie IDs from imdb lists/ratings
+#Returns a list of all IDs
 def get_movies_from_url(url):
 	movie_ids = []
 	soup = get_soup(url)
@@ -18,9 +16,11 @@ def get_movies_from_url(url):
 	while is_there_next_page(soup):
 		soup = get_soup(get_next_page_url(soup))
 		movie_ids.extend(get_movie_ids_from_page(soup))
+		sleep(randint(1,3))
 	return movie_ids
 
 
+#Gets the Beautiful Soup of the page at url
 def get_soup(url):
 	response = get(url)
 	return BeautifulSoup(response.text, 'html.parser')
@@ -35,10 +35,12 @@ def get_movie_ids_from_page(soup):
 	return movie_ids
 
 
+#Gets the url for the next page
 def get_next_page_url(soup):
 	return imdb_url + soup.find('a', class_ = 'flat-button lister-page-next next-page')['href']
 
 
+#Checks whether the list has another page or not
 def is_there_next_page(soup):
 	if soup.find('a', class_ = 'flat-button lister-page-next next-page') is not None:
 		return True
