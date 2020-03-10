eacde22709daf6a1eecea9049d9b021b4d7b508f
Sébastien Chapman <sebastien.chapman@FR-LEV1-MFG0002.local>
2020-03-10 11:41:52 +0100

    Removed not working code

--- get_all_films.py
+++ get_all_films.py
@@ -1,45 +1,26 @@
 from requests import get
 from bs4 import BeautifulSoup
+from time import sleep
+from random import randint
 import re
 import math
 
-url = ('https://www.imdb.com/user/ur51298763/ratings?sort=date_'\
-		'added%2Cdesc&mode=detail&paginationKey=mfq5ijak6z7uymj'\
-		'wuuwsomnsegl34knnqsdztp6xeepepyyfxdfiwpol52uhtjimq3iwc'\
-		'lnm7gq7uk2y4kjyc3qnmn3dio7t7rbxu5m47iyfrvleknv4axfhhxu'\
-		'djs5nyx6ixac3qfdajjg6bqac2wheaznk2ouqhjumdq6vdrv2pd4x3'\
-		'rats7f7appdrvwvo4p2yvgwhiztfo3u6u&lastPosition=0')
+url = ('https://www.imdb.com/user/ur51298763/ratings')
 
 def main():
-	pass
-
-
-#Function that rounds numbers down to the nearest 100
-#Only for positive numbers
-def round_down_nearest_100(number):
-	number = math.floor(number/100.0)*100
-	return number
-
-#Finds the upper bound for the imdb list
-#The upper bound is the number used to 
-#find what the last page containing movies
-#starts with
-def find_upper_bound(number):
-	if number != 0:
-		if number % 100 == 0:
-			number -= 100
-	number = round_down_nearest_100(number)
-	return number
-
+	#movie_ids = []
+	#soup = get_soup(url + '0')
+	#total = get_total_number_of_movies(soup, is_ratings(url))
+	#movie_ids.extend(get_movie_ids_from_page(soup))
+	#if total > 100:
+	#	for i in range(100, total, 100):
+	#		movie_ids.extend(get_movie_ids_from_page(get_soup(url + str(i))))
+	#		sleep(randint(1,3))
+	#print(movie_ids)
 
 def get_soup(url):
 	response = get(url)
 	return BeautifulSoup(response.text, 'html.parser')
-
-#Checks whether the page is a ratings page
-#Or not
-def is_ratings(url):
-	return 'ratings' in url
 
 #Gets all the movie ids from a specific imdb page
 def get_movie_ids_from_page(soup):
@@ -50,13 +31,19 @@ def get_movie_ids_from_page(soup):
 	return movie_ids
 
 #Gets the total number of movies in a list/ratings page
-def get_total_number_of_movies(soup, ratings):
-	if ratings:
-		total_number_of_films = soup.find('span', id = 'lister-header-current-size')
-	else:
-		total_number_of_films = soup.find('div', class_ = 'desc lister-total-num-results')
-	return int(re.sub("[^0-9]", "", total_number_of_films.text))
+#def get_total_number_of_movies(soup, ratings):
+#	if ratings:
+#		total_number_of_films = soup.find('span', id = 'lister-header-current-size')
+#	else:
+#		total_number_of_films = soup.find('div', class_ = 'desc lister-total-num-results')
+#	return int(re.sub("[^0-9]", "", total_number_of_films.text))
 
+def get_next_page_url(soup):
+	pass
+
+def is_there_next_page(soup):
+	pass
 
 if __name__== "__main__":
   main()
+
